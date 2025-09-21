2605ccf71baedcbc8e4fb79c95b27fc68e26dc58
programacion WEB <jasanchezm01@iesarroyoharnina.es>
2025-09-21 13:43:01 +0200

    puesta a punto para teoria del 22 de septiembre

--- GRASP Project clase 26 eclipse/GRASP/src/expertoEnInformacion02/mal/Informe.java
+++
@@ -1,6 +0,0 @@
-package expertoEnInformacion02.mal;
-
-public class Informe {
-
-	public int[] parciales = { 4, 5, 6, 3, 8, 4, 5, 6, 2, 2, 7, 8, 8, 8, 8, 8, 9, 9, 0 };
-}
--- GRASP Project clase 26 eclipse/GRASP/src/expertoEnInformacionTeoria01/mal/Linea.java
+++
@@ -1,7 +0,0 @@
-package expertoEnInformacionTeoria01.mal;
-
-public class Linea {
-	public String nombre;
-	public float precio;
-	public float descuento=0;
-}
